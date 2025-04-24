f49189f7fe7aa2935555e801bf612e0408827ee4
Sublimeful <Qiangwu2000@gmail.com>
2025-04-23 19:39:42 -0500

    Add a popup at the start

--- js/index.js
+++ js/index.js
@@ -1,16 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
+  let veget_btn = document.getElementById("vegetarian-btn");
+  let vegan_btn = document.getElementById("vegan-btn");
 
-    let veget_btn = document.getElementById("vegetarian-btn");
-    let vegan_btn = document.getElementById("vegan-btn")
+  veget_btn.addEventListener("click", () => {
+    localStorage.setItem("preference", "Vegeterian");
+    location.href = "./pages/scanner.html";
+  });
 
-    veget_btn.addEventListener("click", () =>{
-        localStorage.setItem("preference", "Vegeterian");
-        location.href='./pages/scanner.html';
-    });
+  vegan_btn.addEventListener("click", () => {
+    localStorage.setItem("preference", "Vegan");
+    location.href = "./pages/scanner.html";
+  });
 
-    vegan_btn.addEventListener("click", () =>{
-        localStorage.setItem("preference", "Vegan");
-        location.href='./pages/scanner.html';
-    });
-    
+  const popup = document.querySelector(".popup")
+  const close_popup_btn = document.querySelector(".popup > .close-btn")
+
+  close_popup_btn.addEventListener("click", () => {
+    localStorage.setItem("hide-popup", true)
+    popup.style.display = "none";
+  });
+
+  // Show or hide popup
+  if (!localStorage.getItem("hide-popup")) {
+    popup.style.display = "block";
+  }
+
+
 });
